fa34fca600bede7c1c28a3afd771a0fe6b81d0f6
dotocan <dominik.otocan@valamar.com>
2025-04-10 13:12:47 +0200

    choose dates works now

--- tests/SearchAndNavigation.spec/b_Homepage_GlavnaNavigacija_PropertyListing_RoomListing.spec.ts
+++ tests/SearchAndNavigation.spec/b_Homepage_GlavnaNavigacija_PropertyListing_RoomListing.spec.ts
@@ -12,22 +12,25 @@ test('Homepage → Glavna navigacija → Property listing → Room listing', asy
     await page.getByRole('button', { name: 'Accept cookies' }).click();
     await page.getByRole('button', { name: 'Hotels & Resorts' }).click();
     await page.getByRole('link', { name: 'All Hotels & Resorts in Poreč' }).click();
+    // wait for loader container to stop loading
+    let loaderContainer = page.locator('div.loader-container').nth(1);
+    await loaderContainer.waitFor({ state: "hidden" });
 
-    const heading = await page.getByRole('heading', { name: 'Our hotels and apartments in Poreč' });
-    await expect(heading).toBeVisible();
+    const heading = page.getByRole('heading', { name: 'Our hotels and apartments in Poreč' });
+    await heading.waitFor({ state: "visible" });
 
     const allSeeDetailsButtons = page.getByRole('button', { name: 'See details & book' });
     const seeDetailsButtonsCount = await allSeeDetailsButtons.count();
 
     const initialUrl = page.url();
     // for (let i = 0; i < seeDetailsButtonsCount; i++) {
-    for (let i = 18; i < seeDetailsButtonsCount; i++) {
+    for (let i = 19; i < seeDetailsButtonsCount; i++) {
         // set current button
         const button = allSeeDetailsButtons.nth(i);
         await button.waitFor()
 
         // wait for loader container to stop loading
-        let loaderContainer = page.locator('div.loader-container').nth(1);
+        loaderContainer = page.locator('div.loader-container').nth(1);
         await loaderContainer.waitFor({ state: "hidden" });
 
         // click the button
@@ -46,21 +49,8 @@ test('Homepage → Glavna navigacija → Property listing → Room listing', asy
         console.log("prop heading", headingText);
 
         if (headingText == PROPERTY_NAME) {
-            // do something when on Diamant site
-            // selectRate(page);
-            // press the detais of the seaview room
-            const spanDetails = page.locator('span.app-text', { hasText: 'Details' }).nth(1);
-            spanDetails.waitFor();
-            await spanDetails.click();
-
-            // select rate to get to booking step
-            const selectRate = page.getByLabel('Room for 2+1 Seaview').getByRole('button', { name: 'Select Rate' });
-            selectRate.waitFor();
-            await selectRate.click();
-
-            // test out if it went to booking process
-            const heading = await page.getByRole('heading', { name: 'Select your rate' });
-            await expect(heading).toBeVisible();
+            // select rate when on Diamant site
+            await selectRate(page);
             break;
         } else {
             await page.goBack();
@@ -74,9 +64,41 @@ test('Homepage → Glavna navigacija → Property listing → Room listing', asy
 });
 
 async function selectRate(page: any) {
-    const headingElement = page.getByRole('heading', { name: 'Valamar Diamant Hotel' });
-    headingElement.waitFor();
-    expect(headingElement).toBeAttached();
+    // press the detais of the seaview room
+    // TODO: Need to iterate to get to this nth(3)
+    const spanDetails = page.locator('span.app-text', { hasText: 'Details' }).nth(3);
+    spanDetails.waitFor();
+    await spanDetails.click();
+
+    // choose dates
+    await page.locator(
+        'div.dialog-overlay-container:has(h4:has-text("Room for 2")) button:has-text("Choose dates")'
+    ).click();
+
+    // next month
+    await page.getByRole('button').filter({ hasText: /^$/ }).click();
+
+    // check days
+    const firstDay = page.locator('span.day', { hasText: '23' }).nth(1);
+    firstDay.waitFor();
+    const secondDay = page.locator('span.day', { hasText: '26' }).nth(1);
+    secondDay.waitFor();
+    await firstDay.click();
+    await secondDay.click();
+    await page.getByRole('button', { name: 'Confirm' }).click();
+
+    // go to details again
+    spanDetails.waitFor();
+    await spanDetails.click();
+
+    // select rate to get to booking step
+    const selectRate = page.getByLabel('Room for 2+1 Seaview').getByRole('button', { name: 'Select Rate' });
+    selectRate.waitFor();
+    await selectRate.click();
+
+    // test out if it went to booking process
+    const heading = await page.getByRole('heading', { name: 'Select your rate' });
+    await expect(heading).toBeVisible();
 
 
 }
