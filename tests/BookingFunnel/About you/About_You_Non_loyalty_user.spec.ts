836f86bd5bf616f322ae1a5ee2fede27c6c1b4c6
dotocan <dominik.otocan@valamar.com>
2025-04-11 15:23:44 +0200

    discount disabled for about you

--- tests/BookingFunnel/About you/About_You_Non_loyalty_user.spec.ts
+++ tests/BookingFunnel/About you/About_You_Non_loyalty_user.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { url } from 'inspector';
-import { goToCart } from '../../helper-utils/helper-utils';
+import { goToCart, goToCartQuick } from '../../helper-utils/helper-utils';
 
 test('Select your rate - Non-loyalty user', async ({ page }) => {
   // Increase the global test timeout.
   test.setTimeout(120000);
 
   // go to cart page
-  await goToCart(page);
+  await goToCartQuick(page);
 
   // START THE SELECT RATE MODULE
   // console.log('This is the page url', page.url())
   const bookingUrl = page.url();
 
+  await page.locator('.toggle').click();
   await page.locator('#group-2 span').click();
   await page.getByRole('button', { name: 'Make a reservation' }).click();
 
@@ -20,5 +21,21 @@ test('Select your rate - Non-loyalty user', async ({ page }) => {
 
   const aboutYouHeading = page.getByRole('heading', { name: 'About you' });
   await aboutYouHeading.waitFor({ state: "visible" });
-  await page.screenshot({ path: 'aboutYouScreenshot.png', fullPage: true });
+
+  // ABOUT YOU NON LOYALTY USER MODULE STARTS HERE
+
+  await page.getByRole('textbox', { name: 'E-mail address' }).fill('testnimail14231423@gmail.com');
+  await page.getByRole('button', { name: 'I don′t want to specify' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).fill('FirstName');
+  await page.getByRole('textbox', { name: 'Last Name' }).fill('LastName');
+  await page.getByRole('textbox', { name: 'Date of birth' }).fill('01.12.1998y');
+  await page.getByRole('button', { name: 'Country Select country' }).click();
+  await page.getByRole('option', { name: 'Croatia' }).click();
+  await page.getByRole('button', { name: 'Have special requests?' }).click();
+  await page.locator('textarea').click();
+  await page.locator('textarea').fill('ovo je test');
+  await page.getByRole('button', { name: 'Continue to payment' }).click();
+  await page.getByRole('textbox', { name: '+385 Mobile phone number' }).fill('14231423');
+  await page.getByRole('button', { name: 'Continue to payment' }).click();
+
 });
